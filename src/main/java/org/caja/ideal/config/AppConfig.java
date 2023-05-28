54c3c60f737c646cda9b4fca253f713814adf696
Angel Aguero <angelagueror23@gmail.com>
2023-05-28 16:59:45 -0400

    config

--- src/main/java/org/caja/ideal/config/AppConfig.java
+++ src/main/java/org/caja/ideal/config/AppConfig.java
@@ -1,2 +1,33 @@
-package org.caja.ideal.config;public class AppConfig {
+package org.caja.ideal.config;
+
+import org.caja.ideal.models.Roles;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    UserDetailsService userDetails (){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("Angel").password("admin").roles(String.valueOf(Roles.USER)).build());
+        return manager;
+    }
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetails())
+                .passwordEncoder(encoder())
+                .and().build();
+    }
 }
--- src/main/java/org/caja/ideal/security/AppSecurity.java
+++ src/main/java/org/caja/ideal/security/AppSecurity.java
@@ -1,2 +1,33 @@
-package org.caja.ideal.security;public class AppSecurity {
+package org.caja.ideal.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class AppSecurity {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors().disable()
+                .authorizeHttpRequests( (auth) -> {
+                    auth.requestMatchers("/api/add").permitAll();
+                    auth.requestMatchers("/api/hello").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement((session) -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .httpBasic()
+                .and()
+                .build();
+    }
+
+
 }
